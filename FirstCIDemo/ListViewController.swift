63412ec8907bc80adde3d8d9e533e2e329738d63
stlhorizon <anand@stl-horizon.com>
2022-12-07 19:35:37 +0530

    Dev List and Unit Test Cases (#1)
    - Added Dev list
    - Added Test cases

---
+++ FirstCIDemo/ListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FirstCIDemo
+//
+//  Created by Anand Upadhyay on 07/12/22.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    @IBOutlet public var listTable: UITableView!
+    
+    public var devList = [["name":"Thoma Andrew","descignation":"iOS Developer","location":"Texas US"],["name":"Nency Jordan","descignation":"Technical Lead","location":"Sweden"],["name":"fentuia Donzigutia","descignation":"Implementation Head ","location":"Austria"]]
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = listTable.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        let animal = devList[indexPath.row]
+        
+        cell.textLabel?.text = animal["name"]
+        cell.detailTextLabel?.text =  animal["descignation"]! + "\n" + animal["location"]!
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        presenter?.showAnimalSelection(with: animalList[indexPath.row], from: self)
+    }
+    
+}
+
